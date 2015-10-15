bd16c475840daaef90de5c64762fd6077f5c2adc
alexsholk <alex.sholk@itmhouse.com>
2015-10-15 22:19:48 +0300

    Remove unused bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // App bundle:
             new AppBundle\AppBundle(),
+            // Sonata and dependencies:
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            // AxS bundles:
+            new AxS\ConfigBundle\AxSConfigBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
